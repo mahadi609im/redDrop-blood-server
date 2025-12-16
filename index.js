9689d3e028a5ce575d89adfa7b98202fcc9c12bf
mahadi609im <maha609im@gmail.com>
2025-12-16 12:49:08 +0600

    profile update

--- index.js
+++ index.js
@@ -411,7 +411,7 @@ async function run() {
     });
 
     // users collection
-    app.get('/users', verifyFBToken, verifyAdmin, async (req, res) => {
+    app.get('/users', verifyFBToken, async (req, res) => {
       const { email } = req.query;
       const query = {};
 
